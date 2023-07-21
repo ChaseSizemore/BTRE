145e12555d3f102a807a851c6a953eaf568da7b2
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-07-20 23:51:31 -0400

    views, pages, static, dos2unix files with CRLF - LF issue

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from django.shortcuts import render
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+]
