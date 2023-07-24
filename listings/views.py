2c6e024702c158d1762e1794b11d39a38eed760e
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-07-24 12:48:26 -0400

    about and index page

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+# from .choices import price_choices, bedroom_choices, state_choices
+
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'listings/listings.html')
+from .models import Listing
 
-def listing(request):
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings': paged_listings}
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
